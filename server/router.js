5b5b5c0c962d1803675178a3e2e004fa50e9fa2e
pixilated44 <pixilated44@gmail.com>
2018-06-20 14:44:39 -0600

    Update router.js

--- server/router.js
+++ server/router.js
@@ -3,7 +3,7 @@ const ContactList = require('./controllers/contact_list_controller');
 
 module.exports = function(app) {
 	app.get('/', function(req, res) { res.sendFile(path.join(__dirname, '/static/index.html')) });
-	app.get('/success', function(req, res) { res.sendFile(path.join(__dirname, '/static/success.html')) });
+	app.get('/success', function(req, res) { res.redirect('https://www.tradenavigator.com/email-confirmed') });
 	app.post('/confirmEmail', ContactList.sendConfirmation);
 	app.post('/signup', ContactList.addUser);
 }
