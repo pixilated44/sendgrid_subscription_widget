1255955edbdae1974302be5cac41b885086c86e5
Not Me <luftkopf79@bonzermedia.com>
2018-07-17 14:34:44 -0600

    Update settings.js
    Initial configuration for cryptoworx signup form

--- settings.js
+++ settings.js
@@ -1,17 +1,17 @@
 // Change the url to the domain of your app
-exports.url = 'https://tn-email-signup.herokuapp.com/';
+exports.url = 'https://cryptoworx.herokuapp.com/';
 
 exports.senderEmail = "support@tradenavigator.com";
-exports.senderName = "Trade Navigator";
+exports.senderName = "Cryptoworx";
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
-exports.listId = 854819;
+exports.listId = 4439074;
 
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
-exports.templateId = "a1e9c893-57f4-4fc6-9073-65bd92ce005e";
+exports.templateId = "9a57e31f-be7f-4494-95be-6c1c7afa5a85";
 
 // receive an email when a new signup is confirmed
-exports.sendNotification = true;
-exports.notificationEmail = "jstumpf@tradenavigator.com";
+exports.sendNotification = false;
+exports.notificationEmail = "";
