58fdb9f824d3ee0862a1e3a0abde014a0100e16c
Not Me <luftkopf79@bonzermedia.com>
2018-07-18 10:18:18 -0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ exports.senderName = "Trade Navigator";
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
-exports.listId = 854819;
+exports.listId = null;
 
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
